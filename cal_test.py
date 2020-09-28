15c80de9cf212061b309a6d4c1295bdeacc0050a
pratikshap3 <66252269+pratikshap3@users.noreply.github.com>
2020-09-28 17:48:56 +0530

    Update cal_test.py

--- cal_test.py
+++ cal_test.py
@@ -3,8 +3,8 @@ import unittest
 
 class TestCalculator(unittest.TestCase):
     def test_addition(self):
-        result = calculator.add(2, 5)
-        self.assertEqual(result, 7)
+        result = calculator.add(3, 5)
+        self.assertEqual(result, 8)
         
     def test_subtraction(self):
         result = calculator.subtract(5,2)
