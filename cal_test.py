3eba59190c2eee9ddeb54043fd1ac204741e0afc
pratikshap3 <66252269+pratikshap3@users.noreply.github.com>
2020-09-29 17:19:38 +0530

    Update cal_test.py

--- cal_test.py
+++ cal_test.py
@@ -7,8 +7,8 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(result, 6)
         
     def test_subtraction(self):
-        result = cal_code.subtract(4,2)
-        self.assertEqual(result, 2)
+        result = cal_code.subtract(3,2)
+        self.assertEqual(result, 1)
         
         
 if __name__ == '__main__':
